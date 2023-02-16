65eb1f1c06436b4de360d94dc5283d45d7228889
nikolay <nisan89.prots@gmail.com>
2023-02-16 12:17:28 +0600

    airdrop commit

--- tutorial1/script.js
+++ tutorial1/script.js
@@ -1,28 +1,405 @@
-const contractAddress='0x7759fF3877402e9Ec452578caDB2CFF983aDdEE9';
+//const contractAddress='0x743D131E0Cc4093DA33039513d201cF5d305c2F0';
 
-const contractABI=[{"inputs":[{"internalType":"string","name":"_note","type":"string"}],"name":"setNote","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getNote","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}];
+//const contractABI=[{"inputs":[{"internalType":"string","name":"_note","type":"string"}],"name":"setNote","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getNote","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}];
 
 
-let contract;
-let signer;
-  const provider = new ethers.providers.Web3Provider(window.ethereum, 80001); // matic mumbai chain_id
+const contractTokenAddress = '0x743D131E0Cc4093DA33039513d201cF5d305c2F0';
+const contractTokenABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "initialSupply",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "burn",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "subtractedValue",
+				"type": "uint256"
+			}
+		],
+		"name": "decreaseAllowance",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "addedValue",
+				"type": "uint256"
+			}
+		],
+		"name": "increaseAllowance",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "mint",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
 
-  provider.send("eth_requestAccounts", []).then(() => {
+const contractAirdropAddress = '0x4Ee9f39ef453E9ad18Fd1bA92539F9A9F86b1FC0'
+const contractAirdropABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "address[]",
+				"name": "_arrayAddresses",
+				"type": "address[]"
+			}
+		],
+		"name": "airdropTokensWithTransfer",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			}
+		],
+		"name": "airdropTokensWithTransferFrom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "claimDrop",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+];
+
+
+
+let signer
+let contractToken 
+let contractAirdrop
+const provider = new ethers.providers.Web3Provider(window.ethereum, 80001); // matic mumbai chain_id
+
+
+provider.send("eth_requestAccounts", []).then(() => {
     provider.listAccounts().then((accounts) => {
       signer = provider.getSigner(accounts[0]);
-      contract = new ethers.Contract(
-        contractAddress,
-        contractABI,
+
+      contractToken = new ethers.Contract(
+        contractTokenAddress,
+        contractTokenABI,
         signer
-      );
+    )
+
+    contractAirdrop = new ethers.Contract(
+        contractAirdropAddress,
+        contractAirdropABI,
+        signer
+    )
+
     });
   });
 
-  async function getNote() {
-    console.log(await contract.getNote());
-  }
+//   async function getNote() {
+//     console.log(await contract.getNote());
+//   }
   
-  async function setNote() {
-    const note = document.getElementById("inputNote").value;
-    await contract.setNote(note);
-  }
+//   async function setNote() {
+//     const note = document.getElementById("inputNote").value;
+//     await contract.setNote(note);
+//   }
+
+
+
+async function getBalance(){
+    let MyBalance = document.getElementById("balance")
+    MyBalance.innerText = await contractToken.balanceOf(signer.getAddress())
+    console.log(MyBalance.innerHTML);
+}
+
+async function airdropTokensWithTransfer(){
+    //const addresses = document.getElementById("addresses").value;
+    let inputElements = document.getElementById("addresses").value;
+    let inputArray = inputElements.split(',');
+    console.log('inputArray: '+inputArray);
+
+    await contractAirdrop.airdropTokensWithTransfer(inputArray);
+}
